6bf78ce5bc8fad491bc18a4a4c4f5fc08a39a8cd
Madi <abaymadi1@gmail.com>
2022-10-31 17:18:24 +0600

    add styles to app using Bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from turtle import color
 from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -12,6 +13,10 @@ def index():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/css/')
+def css():
+    return render_template('css.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
