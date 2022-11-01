1b4ec7559c408781ff4e0ca00d85468f0165bf0e
Madi <abaymadi1@gmail.com>
2022-11-02 00:16:06 +0600

    connection MuSQL to app

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 Bootstrap(app)
 
+# DB configuration
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
 @app.route('/')
 def index():
-    colors = ['Red', 'Yellow', 'Green']
-    return render_template('index.html', colors=colors)
+    cursor = mysql.connection.cursor()
+    # cursor.execute("INSERT INTO user VALUES(%s)", (['John']))
+    # cursor.connection.commit()
+    result_value = cursor.execute("SELECT * FROM user;")
+    if result_value > 0:
+        users = cursor.fetchall()
+        print(users)
+    return render_template('index.html')
     # return redirect(url_for('about'))
 
 @app.route('/about/')
